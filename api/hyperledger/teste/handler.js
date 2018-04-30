58b836ad201978efd474a5d11ef521e1838fa187
Marcus Ataide <>
2018-04-30 01:45:16 -0300

    First Commit

---
+++ api/hyperledger/teste/handler.js
@@ -0,0 +1,20 @@
+'use strict'
+import * as post from './post/action'
+
+/**
+ * Serverless Module: Lambda Handler
+ * - Your lambda functions should be a thin wrapper around your own separate
+ * modules, to keep your code testable, reusable and AWS independent
+ * - 'serverless-helpers-js' module is required for Serverless ENV var support.  Hopefully, AWS will add ENV support to Lambda soon :)
+ */
+
+// Require Logic
+export default (event, context) => {
+  let behavior = event.requestContext.httpMethod
+  switch (behavior) {
+    case 'POST':
+      return post.respond(event, (error, response) => {
+        return context.done(error, response)
+      })
+  }
+}
---
+++ api/hyperledger/teste/post/action.js
@@ -0,0 +1,26 @@
+'use strict'
+
+import { Api } from '../../../../lib/response'
+const Match = require('../../../../lib/match')
+// const Util = require('../../../lib/util')
+
+// Multi - Create''
+export function respond (event, cb) {
+
+  const data = JSON.parse(JSON.stringify(event.body))
+
+  // if (Util.isEmpty(data['userId'])) {
+  //   return cb(null, Api.errors(401, 'userId is Missing.'))
+  // } else if (Util.isEmpty(data['roomId'])) {
+  //   return cb(null, Api.errors(401, 'roomId is Missing.'))
+  // }
+  //
+  // data['status'] = true
+
+  Match.registerRoom(data).then(data => {
+    return cb(null, Api.response(data))
+  }).catch(err => {
+    return cb(null, Api.errors(200, {5: err['message']}))
+  })
+}
+
